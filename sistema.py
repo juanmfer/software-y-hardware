bb2cea35335fdd898c49a161fc8f5e9c979c74c2
Juan Manuel <69483837+juanmfer@users.noreply.github.com>
2022-11-14 07:51:24 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -78,8 +78,9 @@ print('###   Informacion de Red:')
 if_addrs = psutil.net_if_addrs()
 for interface_name, interface_addresses in if_addrs.items():
     for address in interface_addresses:
-        print(f"    Interfaz: {interface_name}")
+        #print(f"    Interfaz: {interface_name}")
         if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"    Interfaz: {interface_name}")
             print(f"         IP Address: {address.address}")
             print(f"         Netmask: {address.netmask}")
             print(f"         Broadcast IP: {address.broadcast}")
